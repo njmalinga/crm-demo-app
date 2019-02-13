5bd959dc612ad37baa4a90167629a9e5a5a3c3c4
Njabulo Malinga <njmalinga@outlook.com>
2019-02-13 19:05:30 +0200

    added years as customer filter

--- app/controllers/people/index.js
+++ app/controllers/people/index.js
@@ -1,14 +1,16 @@
 import Controller from '@ember/controller';
 import { computed } from '@ember/object'
+import moment from 'moment';
 
 export default Controller.extend({
   people: computed('model',
     'model.@each.id',
     'first_name',
     'last_name',
+    'years_as_customer',
     function() {
       const model = this.model;
-      if (!this.first_name && !this.last_name) {
+      if (!this.first_name && !this.last_name && !this.years_as_customer) {
         return model;
       }
       const filtered = this.filteredPeople.call(this);
@@ -29,6 +31,13 @@ export default Controller.extend({
         filtered = this.filterByLastName.call(this, people);
       }
     }
+    if (this.years_as_customer) {
+      if (filtered) {
+        filtered = this.filterByYearsAsCustomer.call(this, filtered);
+      } else {
+        filtered = this.filterByYearsAsCustomer.call(this, people);
+      }
+    }
     return filtered;
   },
 
@@ -42,6 +51,16 @@ export default Controller.extend({
   filterByLastName(people) {
     let filtered = people.filter(person => {
       return person.lastname.toLowerCase().match(this.last_name.toLowerCase());
+    });
+    return filtered;
+  },
+
+  filterByYearsAsCustomer(people) {
+    let filtered = people.filter(person => {
+      const today = moment();
+      const joining_date = moment(person.joining_date);
+      const duration = moment.duration(today.diff(joining_date)).as('y');
+      return Math.round(duration) === parseInt(this.years_as_customer);
     });
     return filtered;
   },
